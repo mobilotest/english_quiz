06f5a477f865ec83d6639cc82f06c40a7ba3ff49
Odintsov, Sergey <sergio1tsov@gmail.com>
2016-05-29 15:59:51 -0700

    another version of English Quiz

---
+++ app/src/main/java/sergio1tsov/english_quiz/com/englishquiz/Grammar.java
@@ -0,0 +1,128 @@
+package sergio1tsov.english_quiz.com.englishquiz;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Grammar extends Activity {
+
+    private ListView mylistview;
+    private ArrayAdapter<String> listAdapter;
+    private TextView header;
+    private TextView btnMenu;
+    private TextView btnQuiz;
+
+
+
+    private void createCutomActionBarTitle(){
+        this.getActionBar().setDisplayShowCustomEnabled(true);
+        this.getActionBar().setDisplayShowTitleEnabled(false);
+
+        LayoutInflater inflator = LayoutInflater.from(Grammar.this);
+        View v = inflator.inflate(R.layout.custom_action_bar, null);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/freehand.ttf");
+        TextView frag1 = (TextView)v.findViewById(R.id.titleFragment1);
+        frag1.setTypeface(tf);
+        TextView frag2 = (TextView)v.findViewById(R.id.titleFragment2);
+        frag2.setTypeface(tf);
+
+        frag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Grammar.this, MainMenu.class));
+                finish();
+            }
+        });
+        frag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Grammar.this, MainMenu.class));
+                finish();
+            }
+        });
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grammar);
+
+ //       createCutomActionBarTitle();
+
+    btnQuiz = (TextView) findViewById(R.id.btnBeginQuiz);
+    btnMenu = (TextView) findViewById(R.id.btnMainMenu);
+
+// Font:
+        header = (TextView) findViewById(R.id.headerQuiz);
+        Typeface teacherFont = Typeface.createFromAsset(getAssets(),"fonts/freehand.ttf");
+        header.setTypeface(teacherFont);
+        btnQuiz.setTypeface(teacherFont);
+        btnMenu.setTypeface(teacherFont);
+
+
+// List:
+        mylistview = (ListView) findViewById(R.id.lvGrammar);
+
+        listAdapter = new ArrayAdapter<String>(this,
+                R.layout.list_white_text, R.id.list_content, getResources().getStringArray(R.array.arrGrammar));
+        mylistview.setAdapter(listAdapter);
+
+//        mylistview.setTextColor(color.parseColor("#F12345"));
+
+            }
+
+
+
+
+//////
+
+
+
+
+    public void btnBeginQuiz (View v) {startActivity(new Intent(Grammar.this, QuizAnswer.class));}
+    public void mainMenu (View v) {startActivity(new Intent(Grammar.this, MainMenu.class));}
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.grammar, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/sergio1tsov/english_quiz/com/englishquiz/MainMenu.java
@@ -0,0 +1,132 @@
+package sergio1tsov.english_quiz.com.englishquiz;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+public class MainMenu extends Activity {
+
+    private TextView header;
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void createCutomActionBarTitle(){
+        this.getActionBar().setDisplayShowCustomEnabled(true);
+        this.getActionBar().setDisplayShowTitleEnabled(false);
+
+        LayoutInflater inflator = LayoutInflater.from(MainMenu.this);
+        View v = inflator.inflate(R.layout.custom_action_bar, null);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(),"fonts/freehand.ttf");
+        TextView frag1 = (TextView)v.findViewById(R.id.titleFragment1);
+        frag1.setTypeface(tf);
+        TextView frag2 = (TextView)v.findViewById(R.id.titleFragment2);
+        frag2.setTypeface(tf);
+
+        frag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, MainMenu.class));
+                finish();
+            }
+        });
+        frag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, MainMenu.class));
+                finish();
+            }
+        });
+
+        //assign the view to the actionbar
+        this.getActionBar().setCustomView(v);
+    }
+
+
+
+    /*
+    private TextView result;
+    private ImageView smiley;
+    boolean firstTime = true;
+    */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_menu);
+
+// Fonts:
+        header = (TextView) findViewById(R.id.headerQuiz);
+        Typeface teacherFont = Typeface.createFromAsset(getAssets(),"fonts/freehand.ttf");
+        header.setTypeface(teacherFont);
+
+//    exit button
+    ImageButton exitOut = (ImageButton) findViewById(R.id.exitImgButton);
+    exitOut.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+            //System.exit(0);
+        }
+    });
+
+    }
+
+    public void btnFirst_Grammar (View v){
+        startActivity(new Intent(MainMenu.this, Grammar.class));
+    }
+
+    public void btnSecond_Quiz (View v){
+//        firstTime = false;
+        startActivity(new Intent(MainMenu.this, QuizAnswer.class));
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+
+
+    }
+
+/*     @Override
+    protected void onResume() {
+        result = (TextView) findViewById(R.id.rightOrWrong);
+        smiley = (ImageView) findViewById(R.id.imgFace);
+
+       if (!firstTime) {
+            result.setText("GOOD  JOB !!!");
+            smiley.setImageResource(R.drawable.sad);
+        };
+    }
+*/}
