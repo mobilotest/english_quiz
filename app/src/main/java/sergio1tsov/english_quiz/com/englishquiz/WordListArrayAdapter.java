b7cdc5f9db780492c3e12082d190f6cf4bde63aa
WaleedX Basheer <wbasheer@yahoo.com>
2016-06-05 20:57:16 -0700

    - Fixed size issue of text-to-speech icon - Added text-to-speech when user clicks on list item

--- app/src/main/java/sergio1tsov/english_quiz/com/englishquiz/WordListArrayAdapter.java
+++ app/src/main/java/sergio1tsov/english_quiz/com/englishquiz/WordListArrayAdapter.java
@@ -2,13 +2,16 @@ package sergio1tsov.english_quiz.com.englishquiz;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by sodintso on 6/5/16.
@@ -17,11 +20,21 @@ public class WordListArrayAdapter extends BaseExpandableListAdapter {
 
     private Context mContext;
     private ArrayList <String> mWords;
+    private TextToSpeech mTts;
 
     public WordListArrayAdapter(Context context, ArrayList<String> words) {
 
         mContext = context;
         mWords = words;
+
+        mTts = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    mTts.setLanguage(Locale.getDefault());
+                }
+            }
+        });
 
     }
 
@@ -62,7 +75,7 @@ public class WordListArrayAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String word = (String) getGroup(groupPosition);
+        final String word = (String) getGroup(groupPosition);
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) this.mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -73,6 +86,16 @@ public class WordListArrayAdapter extends BaseExpandableListAdapter {
                 .findViewById(R.id.txt_word_list);
         lblListHeader.setTypeface(null, Typeface.BOLD);
         lblListHeader.setText(word);
+
+        ImageView soundImageView = (ImageView) convertView.findViewById(R.id.sound_on);
+        soundImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String toSpeak = ed1.getText().toString();
+                //Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                mTts.speak(word, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
 
         return convertView;
 
@@ -89,12 +112,20 @@ public class WordListArrayAdapter extends BaseExpandableListAdapter {
             convertView = infalInflater.inflate(R.layout.layout_child_word_list, null);
         }
 
-        TextView txtListChild = (TextView) convertView
-                .findViewById(R.id.txt_child);
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.txt_child);
 
         txtListChild.setText("placeholder");
         return convertView;
     }
+
+//    private View.OnClickListener onSoundImageClicked = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            String toSpeak = ed1.getText().toString();
+//            //Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+//            t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+//        }
+//    }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
